099870a411d4ff1f8d8e1cfa8ccad93a7f287ce8
Shahriar Farzam <sh.farzam@hotmail.com>
2023-11-24 00:21:03 +0100

    add test functions

--- tests/ValidatorTest.php
+++ tests/ValidatorTest.php
@@ -1,8 +1,19 @@
 <?php
 
+use DataValidation\Validator;
 use PHPUnit\Framework\TestCase;
 
 class ValidatorTest extends TestCase
 {
-    // Test cases for validation methods will be written here
+    public function testEmailValidation()
+    {
+        $this->assertTrue(Validator::validateEmail('example@example.com'));
+        $this->assertFalse(Validator::validateEmail('invalid_email'));
+    }
+
+    public function testURLValidation()
+    {
+        $this->assertTrue(Validator::validateURL('https://www.example.com'));
+        $this->assertFalse(Validator::validateURL('invalid_url'));
+    }
 }
