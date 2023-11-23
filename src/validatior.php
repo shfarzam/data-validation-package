4eb7b6ceddc4046a19eb55de62c0b76fe96d7623
Shahriar Farzam <sh.farzam@hotmail.com>
2023-11-24 00:20:29 +0100

    add validateEmail add validateURL add validatePassword

--- src/validatior.php
+++ src/validatior.php
@@ -1,7 +1,24 @@
 <?php
 
+namespace DataValidation;
+
 class Validator
 {
 
+    public static function validateEmail($email)
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
+    }
+
+    public static function validateURL($url)
+    {
+        return filter_var($url, FILTER_VALIDATE_URL) !== false;
+    }
+
+    public static function validatePassword($password, $minLength = 8)
+    {
+        // Add password validation logic (e.g., length, complexity checks)
+        // Return true if the password meets the criteria, false otherwise
+    }
 
 }
